1cabcd3ebe47ea68689a31fcf442b18a08b9b806
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-10-07 12:50:53 +0200

    add logout and token refresh url

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegistrationView, ActivationView, LoginView
+from .views import RegistrationView, ActivationView, LoginView, LogoutView, TokenRefreshView
 
 app_name = 'auth_app'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('activate/<uidb64>/<token>/', ActivationView.as_view(), name='activate'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
