f25d9bc5d85201784264e5a04dedf973ccad4f12
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-10-09 11:45:28 +0200

    change path in video model

--- content/models.py
+++ content/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 CATEGORY_CHOICES = {
     ("drama", "Drama"),
     ("romance", "Romance"),
 }
 
+
 class Video(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=155)
     description = models.CharField(max_length=255)
     thumbnail_url = models.URLField()
-    category = models.CharField(max_length=55, choices=CATEGORY_CHOICES, null=True, blank=True)
-    video_file = models.FileField(upload_to='videos/')
+    category = models.CharField(
+        max_length=55, choices=CATEGORY_CHOICES, null=True, blank=True)
+    video_file = models.FileField(upload_to=f"videos/")
 
     def __str__(self):
         return f"{self.title} | Category: {self.category or 'Uncategorized'} | Uploaded: {self.created_at:%Y-%m-%d %H:%M}"
