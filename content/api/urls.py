e5f191ec62965a17d1b9c256d1dae604100fa5e4
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-10-10 18:33:23 +0200

    add video playback url

--- content/api/urls.py
+++ content/api/urls.py
@@ -3,4 +3,5 @@ from content.api.views import VideoListView
 
 urlpatterns = [
     path('video/', VideoListView.as_view(), name='video-list'),
+    path('video/<int:movie_id>/<str:resolution>/index.m3u8/', name='video')
 ]
