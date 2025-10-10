39901ea2b91e2e99a0f439bf8a081a21cece1b05
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-10-10 09:55:08 +0200

    add post delete function in signals

--- content/signals.py
+++ content/signals.py
@@ -1,15 +1,38 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 import django_rq
+import os
 
 from content.utils import convert_video
 from .models import Video
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
-    print('Video wurde gespeichert')
+    print('Video saved')
     if created:
         print("converting...")
         django_rq.enqueue(convert_video, instance.video_file.path, "hd1080")
         django_rq.enqueue(convert_video, instance.video_file.path, "hd720")
         django_rq.enqueue(convert_video, instance.video_file.path, "hd480")
+
+@receiver(post_delete, sender=Video)
+def video_post_delete(sender, instance, **kwargs):
+    print('Video was delete')
+    if instance.video_file and os.path.isfile(instance.video_file.path):
+        video_path = instance.video_file.path
+        video_directory = os.path.dirname(video_path)
+        video_filename_base = os.path.splitext(os.path.basename(video_path))[0]
+
+        os.remove(video_path)
+
+        search_suffix = ['hls', 'mp4']
+
+        for suffix in search_suffix:
+            dir = os.path.join(video_directory, suffix)
+            if os.path.isdir(dir):
+                for root, _, files in os.walk(dir):
+                    for file in files:
+                        if video_filename_base in file:
+                            file_path_to_delete = os.path.join(root, file)
+                            if os.path.isfile(file_path_to_delete):
+                                os.remove(file_path_to_delete)
