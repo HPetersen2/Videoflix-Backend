077d27a54fce46cdbbac65989099419168752d61
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-10-10 08:17:22 +0200

    add enqueue for convert video upload

--- content/signals.py
+++ content/signals.py
@@ -1,11 +1,15 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+import django_rq
 
-from content.tasks import convert_480p
+from content.utils import convert_video
 from .models import Video
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
     print('Video wurde gespeichert')
     if created:
-        pass
+        print("converting...")
+        django_rq.enqueue(convert_video, instance.video_file.path, "hd1080")
+        django_rq.enqueue(convert_video, instance.video_file.path, "hd720")
+        django_rq.enqueue(convert_video, instance.video_file.path, "hd480")
