6192f5a41008ed3110e3a9c78431ecbe23340b51
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-10-11 15:32:17 +0200

    add permission authenticated with cookie

---
+++ content/api/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework.permissions import BasePermission
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+
+class IsAuthenticatedWithCookie(BasePermission):
+    def has_permission(self, request, view):
+        jwt_authenticator = JWTAuthentication()
+
+        try:
+            validated_token = jwt_authenticator.get_validated_token(request.COOKIES.get('access_token'))
+            user = jwt_authenticator.get_user(validated_token)
+
+            request.user = user
+        
+            return user is not None and user.is_authenticated
+        
+        except(InvalidToken, TokenError):
+            return False
+        except Exception:
+            return False
