8d3390f9420c802b21b6c418f970598fc3183911
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-10-11 15:32:35 +0200

    add load video view

--- content/api/urls.py
+++ content/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from content.api.views import VideoListView
+from content.api.views import VideoListView, VideoView, VideoSegmentView
 
 urlpatterns = [
     path('video/', VideoListView.as_view(), name='video-list'),
-    path('video/<int:movie_id>/<str:resolution>/index.m3u8/', name='video')
+    path('video/<int:movie_id>/<str:resolution>/index.m3u8/', VideoView.as_view(), name='video'),
+    path('video/<int:movie_id>/<str:resolution>/<str:segment>/', VideoSegmentView.as_view(), name='video'),
 ]
--- content/api/views.py
+++ content/api/views.py
@@ -1,9 +1,29 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from content.models import Video
+from content.utils import get_video_file
+from .permissions import IsAuthenticatedWithCookie
 from .serializers import VideoListSerializer
 
 class VideoListView(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedWithCookie]
     queryset = Video.objects.all()
     serializer_class = VideoListSerializer
+
+class VideoView(APIView):
+    permission_classes = [IsAuthenticatedWithCookie]
+
+    def get(self, **kwargs):
+        movie_id = kwargs.get("movie_id")
+        resolution = kwargs.get("resolution")
+
+        return get_video_file(movie_id, resolution=resolution)
+    
+class VideoSegmentView(APIView):
+    permission_classes = [IsAuthenticatedWithCookie]
+
+    def get(self, **kwargs):
+        movie_id = kwargs.get("movie_id")
+        segment = kwargs.get("segment")
+
+        return get_video_file(movie_id, segment=segment)
