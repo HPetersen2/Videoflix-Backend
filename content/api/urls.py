9f974c35b39621d6c06d72e779d5a38b959580c0
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-10-20 18:22:29 +0200

    fix bug url

--- content/api/urls.py
+++ content/api/urls.py
@@ -3,6 +3,6 @@ from content.api.views import VideoListView, VideoView, VideoSegmentView
 
 urlpatterns = [
     path('video/', VideoListView.as_view(), name='video-list'),
-    path('video/<int:movie_id>/<str:resolution>/index.m3u8/', VideoView.as_view(), name='video'),
-    path('video/<int:movie_id>/<str:resolution>/<str:segment>/', VideoSegmentView.as_view(), name='video'),
+    path('video/<int:movie_id>/<str:resolution>/index.m3u8', VideoView.as_view(), name='video'),
+    path('video/<int:movie_id>/<str:resolution>/<str:segment>', VideoSegmentView.as_view(), name='video'),
 ]
